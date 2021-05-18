99ef8395cfe8285379aef8f5c3cf86b27fc9f60a
William Hilton <wmhilton@gmail.com>
2021-05-18 00:52:42 -0400

    initial code

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,63 +1,44 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { ByteArray, Bytes } from "@graphprotocol/graph-ts"
 import {
   ERC721Token,
-  Approval,
-  ApprovalForAll,
   Transfer
 } from "../generated/ERC721Token/ERC721Token"
-import { ExampleEntity } from "../generated/schema"
+import { Contract } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+let ERC721_INTERFACE = ByteArray.fromHexString('80ac58cd') as Bytes;
+let ERC721_METADATA_INTERFACE = ByteArray.fromHexString(('5b5e139f')) as Bytes;
+let ERC721_ENUMERABLE_INTERFACE = ByteArray.fromHexString(('780e9d63')) as Bytes;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._owner = event.params._owner
-  entity._approved = event.params._approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+function supportsInterface(contract: ERC721Token, interfaceId: Bytes): boolean {
+  let result = contract.try_supportsInterface(interfaceId);
+  return !result.reverted && result.value;
 }
 
-export function handleApprovalForAll(event: ApprovalForAll): void {}
+export function handleTransfer(event: Transfer): void {
+  let entity = Contract.load(event.address.toHex());
 
-export function handleTransfer(event: Transfer): void {}
+  // We only insert the entry the very first time, we won't update it.
+  // Yes, that means the `totalSupply` value will be pretty useless for new contracts.
+  // This is so we can avoid interacting with the smart contract at all if possible,
+  // to reduce running time to as little as possible.
+  if (entity !== null) return;
+
+  let contract = ERC721Token.bind(event.address);
+
+  if (supportsInterface(contract, ERC721_INTERFACE)) {
+    entity = new Contract(event.address.toHex())
+
+    if (supportsInterface(contract, ERC721_METADATA_INTERFACE)) {
+      let name = contract.try_name();
+      if (name.value !== null) entity.name = name.value;
+      let symbol = contract.try_symbol();
+      if (symbol.value !== null) entity.symbol = symbol.value;
+    }
+
+    if (supportsInterface(contract, ERC721_ENUMERABLE_INTERFACE)) {
+      let totalSupply = contract.try_totalSupply()
+      if (totalSupply.value !== null) entity.totalSupply = totalSupply.value;
+    }
+    entity.save();
+  }
+}
