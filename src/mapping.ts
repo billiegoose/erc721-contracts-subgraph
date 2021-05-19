88320ce1f4f6a600a6fb39dfc8a9143367786cd4
William Hilton <wmhilton@gmail.com>
2021-05-19 19:37:44 -0400

    a rather ambitious upgrade

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { ByteArray, Bytes } from "@graphprotocol/graph-ts"
+import { Address, ByteArray, Bytes, BigInt } from "@graphprotocol/graph-ts"
 import {
   ERC721Token,
   Transfer
@@ -7,38 +7,53 @@ import { Contract } from "../generated/schema"
 
 let ERC721_INTERFACE = ByteArray.fromHexString('80ac58cd') as Bytes;
 let ERC721_METADATA_INTERFACE = ByteArray.fromHexString(('5b5e139f')) as Bytes;
-// let ERC721_ENUMERABLE_INTERFACE = ByteArray.fromHexString(('780e9d63')) as Bytes;
+let NULL_ADDRESS = Address.fromHexString('0000000000000000000000000000000000000000');
+let BIG_ONE = BigInt.fromI32(1);
 
 function supportsInterface(contract: ERC721Token, interfaceId: Bytes): boolean {
   let result = contract.try_supportsInterface(interfaceId);
   return !result.reverted && result.value;
 }
 
+function isNullAddress(address: Address): boolean {
+  return address.equals(NULL_ADDRESS);
+}
+
 export function handleTransfer(event: Transfer): void {
   let entity = Contract.load(event.address.toHex());
 
-  // We only insert the entry the very first time, we won't update it.
-  // Yes, that means the `totalSupply` value will be pretty useless for new contracts.
-  // This is so we can avoid interacting with the smart contract at all if possible,
-  // to reduce running time to as little as possible.
-  if (entity !== null) return;
+  if (entity === null) {
+    // Note: we only load the Contract the first time
+    // This is so we can avoid interacting with the smart contract at all if possible,
+    // to reduce running time to as little as possible.
+    let contract = ERC721Token.bind(event.address);
 
-  let contract = ERC721Token.bind(event.address);
-
-  if (supportsInterface(contract, ERC721_INTERFACE)) {
-    entity = new Contract(event.address.toHex())
-
-    if (supportsInterface(contract, ERC721_METADATA_INTERFACE)) {
-      let name = contract.try_name();
-      if (name.value !== null) entity.name = name.value;
-      let symbol = contract.try_symbol();
-      if (symbol.value !== null) entity.symbol = symbol.value;
+    if (supportsInterface(contract, ERC721_INTERFACE)) {
+      entity = new Contract(event.address.toHex())
+      entity.totalSupply = BigInt.fromI32(0);
+      entity.firstTransferTimestamp = event.block.timestamp;
+  
+      if (supportsInterface(contract, ERC721_METADATA_INTERFACE)) {
+        let name = contract.try_name();
+        if (name.value !== null) entity.name = name.value;
+        let symbol = contract.try_symbol();
+        if (symbol.value !== null) entity.symbol = symbol.value;
+      }
+  
+    } else {
+      return;
     }
-
-    // if (supportsInterface(contract, ERC721_ENUMERABLE_INTERFACE)) {
-    //   let totalSupply = contract.try_totalSupply()
-    //   if (totalSupply.value !== null) entity.totalSupply = totalSupply.value;
-    // }
-    entity.save();
   }
+
+  let totalSupply = entity.totalSupply;
+
+  if (isNullAddress(event.params._from) /* mint */) {
+    entity.totalSupply = totalSupply.plus(BIG_ONE);
+  } else if (isNullAddress(event.params._to) /* burn */) {
+    entity.totalSupply = totalSupply.minus(BIG_ONE);
+  }
+
+  entity.lastTransferTimestamp = event.block.timestamp;
+
+  entity.save();
 }
